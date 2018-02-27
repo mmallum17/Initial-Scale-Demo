1ea8434f71523eb63accf78f03c6ed9303353896
Marcus <marcusmallum@gmail.com>
2018-02-27 02:47:57 -0600

    TCP Server implemented

--- frontend-server.js
+++ frontend-server.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
--- mcu-server.js
+++ mcu-server.js
@@ -1,8 +1,32 @@
-var io = require('socket.io-client');
-var socket = io.connect('http://18.221.30.192:80', {reconnect: true});
+// Initialize Modules
+const net = require('net');
+const io = require('socket.io-client');
 
-// Add a connect listener
-socket.on('connect', function (socket) {
-    console.log('Connected!');
+// Start TCP Server for MCU
+const server = net.createServer((client) => {
+    // On Scale Connected, start WebSocket connection to web app server
+    console.log('Scale Connected!');
+    let socket = io.connect('http://18.221.30.192:80', {reconnect: true});
+    socket.on('connect', function (socket) {
+        console.log('Connected to front-end server!');
+    });
+
+    // MCU Connection Events
+    client.on('data', function (chunk) {
+        console.log(chunk.toString());
+    });
+    client.on('end', () => {
+        console.log('Scale Disconnected!');
+        socket.close();
+    });
 });
-socket.emit('CH01', 'me', 'test msg');
+
+// If error occurs, handle accordingly
+server.on('error', (err) => {
+    throw err;
+});
+
+// Listen for MCU
+server.listen(4000, () => {
+    console.log('TCP Server Listening on Port 4000');
+});
